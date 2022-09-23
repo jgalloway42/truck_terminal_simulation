9236cf32df03e481693947f7789d5a6b1dbad610
jgalloway42 <jgallow2@gmail.com>
2022-09-23 15:08:58 -0400

    first commit, working simulation

---
+++ README.md
@@ -0,0 +1,6 @@
+# SimPy-based Simulation of Truck Loading or Unloading Station
+Basic discrete event simulation adapted from SimPy tutorials
+
+### References
+[SimPy In 10 Minutes Doc](https://simpy.readthedocs.io/en/latest/simpy_intro/index.html)
+
---
+++ preamble.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('ggplot')
+plt.rcParams["figure.figsize"] = (8,7)
+pd.set_option('display.max_columns',50)
+np.set_printoptions(precision=3)
